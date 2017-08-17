7b7109f6e600c8acf743e7135f215dbd2ffbe92a
Abel <abelberhane@gmail.com>
2017-08-17 10:41:02 -0400

    Added the functionality to send Images as well

--- FirstTwitterAPITest/Program.cs
+++ FirstTwitterAPITest/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using TweetSharp;
 using System.Timers;
 using System.Net;
+using System.IO;
 
 // Needed to add the System.Net, System.Timers, and TweetSharp for this project.
 namespace FirstTwitterAPITest
@@ -13,19 +14,26 @@ namespace FirstTwitterAPITest
     class Program
     {
         // This is where the API and all other keys are
-        private static string customer_key = "t7bnPyIiQ1XMyfP6ANdd72Epj";
-        private static string customer_key_secret = "tfMApWlO5WKL1bEcGGQYjeq8wWp6WZRQtHzV2IgRd85Fu4qV85";
-        private static string access_token = "134319035-vQxaXfDqzwVIABibani8lxMOePxO3ov7MOxlV72M";
-        private static string access_token_secret = "qfVdkw2fxMHxNMjvh31xNlNnJFS9lND4enoqly78bimzC";
+        private static string customer_key = "gltLPKE3ejALS6qNluvLrjMAA";
+        private static string customer_key_secret = "aeaROPF7K1sRGbklJOx8c8uxfftb3vfGlxO8KNqqQ7GBiEx7hK";
+        private static string access_token = "134319035-8M1bhLCv2HY4qAjKbXeGki4PsPDpqNQgMJuiqnLU";
+        private static string access_token_secret = "GqCDv2A04Z7bOrsAjRHyjt7G8bq1JjN25MXxDKIQQYZ2V";
 
         private static TwitterService service = new TwitterService(customer_key, customer_key_secret, access_token, access_token_secret);
 
-        // This is what is shown in the main console when the program is run 
+        // Created a list and assigned the first spot to the image
+        private static int currentImageID = 0;
+        private static List<string> imageList = new List<string> { $"C:/Users/Abel/Desktop/ConsoleAppTwitterAPI/Images/westworld.jpg" };
+
+
+
+        // This is what is shown in the main console when the program is run. Also holds the fun intro text.
         static void Main(string[] args)
         {
+            Console.WriteLine($"<{DateTime.Now}> - Twittertron 3000 Activating");
             Console.WriteLine($"<{DateTime.Now}> - Loading Awesome Protocol");
             Console.WriteLine($"<{DateTime.Now}> - Twitter Bot Started");
-            SendTweet("Hello World!");
+            SendMediaTweet("Hello World! Twittertron 3000 is activated.", currentImageID);
             Console.Read();
         }
 
@@ -50,13 +58,33 @@ namespace FirstTwitterAPITest
              });
         }
 
-        // This will be the update that I add, sending Media Images
+        // Creates a dictionary item out of our image and sends out the image in the 0 slot
         private static void SendMediaTweet(string _status, int imageID)
         {
+            using (var stream = new FileStream(imageList[imageID], FileMode.Open))
+            {
+                service.SendTweetWithMedia(new SendTweetWithMediaOptions
+                {
+                    Status = _status,
+                    Images = new Dictionary<string, Stream> { { imageList[imageID], stream } }
+                });
 
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"<{DateTime.Now}> - The Package has left the building!");
+                Console.ResetColor();
+
+                if((currentImageID + 1) == imageList.Count)
+                    {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("<Twittertron 3000> - End of Image Array");
+                    Console.ResetColor();
+                    currentImageID = 0;
+                }
+                else
+                {
+                    currentImageID++;
+                }
+            }
         }
-
-
-
     }
 }
